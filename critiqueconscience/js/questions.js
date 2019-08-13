627cee2f6eedde92c7a6237dde70bdaa099c33cc
Lucas Terra <hey.lucasterra@gmail.com>
2019-08-13 10:14:56 -0300

    Update questions.js

--- critiqueconscience/js/questions.js
+++ critiqueconscience/js/questions.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var quotes = ["Who agrees, who disagrees, who silently condones with this system? Who’s ignorant of it?", "What are the long-term effects of this?", "What external forces are at work in this system", "How can this system be misused? Can it be used to cheat, to steal, to confuse, to polarize, to alienate, to dominate, to terrify?", "We tend to treat symptoms, rather than causes.\n Is this the “cure” for something? What causes this something your solution tries to address?", "Who might want to misuse it? What could they gain by it?", "Who is (un)intentionally outside of this system? What do they think of it?"];
+  var quotes = ["Who agrees, who disagrees, who silently condones with this system? Who’s ignorant of it?", "What are the long-term effects of this?", "What external forces are at work in this system?", "How can this system be misused? Can it be used to cheat, to steal, to confuse, to polarize, to alienate, to dominate, to terrify?", "How do people with disabilities use your design?", "We tend to treat symptoms, rather than causes. \n Is your solution the “cure” for something? What causes this “something” your solution tries to address?", "Who might want to misuse it? What could they gain by it?", "Who is (un)intentionally outside of this system? What do they think of it?"];
 
   $('.factbutton').click(function() {
 
