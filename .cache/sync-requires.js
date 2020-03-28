5f56786c47e19647bd8c428719a6a09d505cae09
lucas terra <hey.lucasterra@gmail.com>
2020-03-28 14:21:01 -0300

    cache update

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,6 +6,7 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---src-templates-post-js": hot(preferDefault(require("/Users/lucasterra/heylucasterra.github.io/src/templates/post.js"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/lucasterra/heylucasterra.github.io/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/lucasterra/heylucasterra.github.io/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/lucasterra/heylucasterra.github.io/src/pages/index.js"))),
   "component---src-pages-page-2-js": hot(preferDefault(require("/Users/lucasterra/heylucasterra.github.io/src/pages/page-2.js")))
