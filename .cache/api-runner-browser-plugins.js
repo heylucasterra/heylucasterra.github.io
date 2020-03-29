976d6a3816af05b1ba7f07f1094442f481d1cdd4
lucas terra <hey.lucasterra@gmail.com>
2020-03-29 13:35:56 -0300

    Updates

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-remark-images/gatsby-browser.js'),
-      options: {"plugins":[],"maxWidth":"960px"},
-    },{
-      plugin: require('../gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
