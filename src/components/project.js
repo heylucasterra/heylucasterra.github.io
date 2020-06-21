4d88992e8f3a96ad2639c39913a8fa7524d76134
lucas terra <hey.lucasterra@gmail.com>
2020-06-21 19:15:32 -0300

    Images + chargeback post creation

--- src/components/project.js
+++ src/components/project.js
@@ -1,76 +1,49 @@
 import React from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
 import { motion } from "framer-motion";
 
-const textMotion = {
-  rest: {
-    color: "grey",
-    x: 0,
-    transition: {
-      duration: 2,
-      type: "tween",
-      ease: "easeIn"
+
+const shape = {
+    rest: {
+        y: 0,
+        scale: 1,
+        transition: {
+            duration: 0.5,
+        },
+    },
+    hover: {
+        y: 16,
+        scale: 1.1,
+        transition: {
+            duration: 0.5,
+        },
+    },
+}
+
+const image = {
+    rest: {
+        y:0,
+    },
+    hover: {
+        y:-16,
     }
-  },
-  hover: {
-    color: "blue",
-    x: 0,
-    rotate: 360,
-    transition: {
-      duration: 4,
-      type: "tween",
-      ease: "easeOut"
-    }
-  }
-};
+}
 
-const slashMotion = {
-  rest: { opacity: 0, ease: "easeOut", duration: 0.2, type: "tween" },
-  hover: {
-    opacity: 1,
-    transition: {
-      duration: 0.4,
-      type: "tween",
-      ease: "easeIn"
-    }
-  }
-};
+// const HoverTest = () => {
+//     return (
+//         <Container
+//             initial="rest"/>
+//     )
+// }
 
-const HoverTest = () => {
-  return (
-    <Container initial="rest" whileHover="hover" animate="rest">
-      <SlashContainer variants={slashMotion}>
-        <svg width="1em" height="1em" viewBox="0 0 27 50">
-          <path
-            fill="#154FFF"
-            d="M21.177 0L0 50h5.818L26.995 0z"
-            fillRule="evenodd"
-          />
-        </svg>
-      </SlashContainer>
-      <motion.h1 variants={textMotion}>Hover me!</motion.h1>
-    </Container>
-  );
-};
-export default HoverTest;
 
-const Container = styled(motion.div)`
-  position: relative;
-  max-width: 200px;
-  cursor: pointer;
-`;
-
-const SlashContainer = styled(motion.div)`
-  position: absolute;
-  top: 50%;
-  left: 0;
-  opacity: 0;
-
-  transform: translateY(-50%);
-
-  svg {
-    width: auto;
-    height: 50px;
-    object-fit: scale-down;
-  }
-`;
+{/* <a href="http://lucasterra.com" class="thumbnail-wrapper" style={{height:504, width:255, marginLeft:12}}>
+        <motion.div class="thumbnail">  
+          <motion.img class="thumbnail-image" alt="Nubank Design Team Logo" whileHover={{rotate: 180, transition: {duration: 40 },}} />
+          <motion.svg style={{display:`flex`, alignItems:'flex-end', position:`relative`, bottom:`-4%`,}} xmlns="http://www.w3.org/2000/svg" width="255" height="187" viewBox="0 0 255 187" fill="none" whileHover={{ scale: 1.1, transition: { duration: 0.5 },}} whileTap={{ scale: 1}}>
+            <path d="M127.5 0L255 187H0L127.5 0Z" fill="var(--accent-orange)"/>
+          </motion.svg>
+        </motion.div>
+        <h4 style={{marginBottom:4}}>Nurturing Design Culture</h4>
+        <p>Growing an international design organisation</p>
+      </a> */}
